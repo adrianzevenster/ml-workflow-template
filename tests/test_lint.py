90030af62a64defe33f2d106612e9185b695a8e2
adrianzevenster <a.zevenstar@gmail.com>
2025-04-18 21:29:12 +0200

    Add Flake* Linting config and pytest-based lint test

---
+++ tests/test_lint.py
@@ -0,0 +1,9 @@
+import subprocess
+
+def test_flake8_clean():
+    """Fail if any errors found"""
+    res = subprocess.run(
+        ["flake8", "scripts", "ml-workflow-template"],
+        capture_output=True, text=True
+    )
+    assert res.returncode == 0, f"Lint errors:\n{res.stdout}"
